433ce64bf5c566070c82ef4ffefe487bea2ae717
milosristic124 <kungfupo124@gmail.com>
2018-09-19 21:36:00 +0200

    Auth details

---
+++ src/screens/AuthDetails.js
@@ -0,0 +1,219 @@
+import React, { Component } from 'react'
+import {
+    StyleSheet, Text, View, Image,
+    TouchableWithoutFeedback, StatusBar,
+    TextInput, SafeAreaView, Keyboard, TouchableOpacity,
+    KeyboardAvoidingView, Linking
+} from 'react-native';
+import { LinearGradient } from 'expo';
+import { createStackNavigator } from 'react-navigation';
+import { connect } from 'react-redux';
+import { fetchApi } from '../services/api/index';
+
+class AuthDetailsScreen extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        request_id: "Etheroll",
+        action: null,
+        ethAmount: 0.5,
+        logo_url: "../assets/0x-icon.png",
+        hoursLeft: 24,
+        app_url: 'https://etheroll.com/',
+        item: {}
+      }
+    }
+
+    onApprove = async () => {
+      await this.setState({
+        formData: {
+          session_id: this.props.session_id,
+          sig: 'test',
+          action: 'approve'
+        }
+      });
+
+      fetchApi({
+        url: 'auth/process',
+        payload: this.state.formData,
+        method: 'post',
+      })
+        .then(response => {
+            console.log('Response-->', response);
+            this.props.navigation.navigate('AuthList', { userName: this.props.userName});
+        })
+        .catch(e => {
+            this.setState({
+                loading: false,
+                errors: true,
+            });
+        });
+    }
+
+
+    componentDidMount() {
+        let date = new Date(this.props.navigation.state.params.item.modified * 1000);
+        var time = this.getTime(date);
+        var _date = this.getDate(date);
+        this.setState({
+            status: this.props.navigation.state.params.item.status,
+            date: _date + ' ' + time,
+        })
+    }
+
+    Capitalize(str){
+        return !str ? null : str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    getTime = (date) => {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes +  ampm;
+        return strTime;
+    }
+
+    getDate = (date) => {
+        var _date = date.getDate();
+        var month = date.getMonth() + 1; //Be careful! January is 0 not 1
+        var year = date.getFullYear();
+        var strDate = _date + '.' + month + '.' + year;
+        return strDate;
+    }
+
+    render() {
+      return (
+          <LinearGradient  colors={['#0499ED', '#0782c6', '#1170a3']} style={styles.container}>
+              <StatusBar barStyle="light-content" />
+
+                <View style={styles.logoContainer}>
+                  <Image style={styles.image} source={require("../assets/0x-icon.png")}
+                  />
+                </View>
+
+                <View style={styles.infoContainer}>
+
+                  <Text style={styles.title}>{this.state.request_id}</Text>
+
+
+                  <Text style={styles.explanation}> Requested the following </Text>
+                  <Text style={styles.explanation}> Permissions: </Text>
+
+                  <View style={styles.details}>
+                    <Text style={styles.ethAmount}> Spend up to {this.state.ethAmount} ETH </Text>
+                    <Text style={styles.hoursLeft}> Expires in {this.state.hoursLeft} hours </Text>
+                  </View>
+
+                  <Text style={styles.status}> {this.Capitalize(this.state.status)}</Text>
+                  <Text style={styles.explanation}> {this.state.date}</Text>
+
+                </View>
+
+                <View style={styles.bothButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={this.onApprove}>
+                    <Text style={styles.buttonText}>Cancel Permission</Text>
+                  </TouchableOpacity>
+                </View>
+
+
+          </LinearGradient>
+        )
+      }
+    }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1899cc',
+        flexDirection: 'column',
+    },
+    logoContainer: {
+        alignItems: 'center',
+        marginTop: '25%',
+    },
+    infoContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 20,
+    },
+    title: {
+        color: 'white',
+        fontSize: 23,
+        textAlign: 'center',
+        marginTop: 5,
+        paddingBottom: 20,
+    },
+    explanation: {
+      textAlign: 'center',
+      color: 'white',
+      fontSize: 15,
+      fontWeight: '400'
+    },
+    details: {
+      paddingTop: 30,
+      paddingBottom: 30,
+    },
+    ethAmount: {
+      color: 'white',
+      fontSize: 21,
+      textAlign: 'center',
+    },
+    hoursLeft: {
+      color: 'white',
+      marginTop: 15,
+      fontSize: 20,
+      fontWeight: '300',
+      textAlign: 'center',
+    },
+    explanation_url: {
+      textAlign: 'center',
+      color: 'white',
+      fontWeight: '700'
+    },
+    bothButtonContainer: {
+      paddingTop: 50,
+    },
+    buttonContainer: {
+        backgroundColor: 'white',
+        marginLeft: '20%',
+        marginRight: '20%',
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginBottom: 20,
+        borderRadius: 20,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color :'black',
+        fontWeight: '300',
+        fontSize: 18
+    },
+    image: {
+      height: 100,
+      width: 100,
+    },
+    status: {
+        marginTop: 50,
+        paddingBottom: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '400'
+    },
+})
+
+const mapStateToProps = (state) => ({
+  nav: state.nav,
+  isLoggedIn: state.auth.isLoggedIn,
+  pubkey: state.auth.pubkey,
+  userName: state.auth.userName,
+  session_id: state.auth.session_id,
+});
+
+export default connect(mapStateToProps)(AuthDetailsScreen);
--- src/screens/AuthList.js
+++ src/screens/AuthList.js
@@ -3,36 +3,48 @@ import {
     StyleSheet, Text, View, Image,
     TouchableWithoutFeedback, StatusBar,
     TextInput, SafeAreaView, Keyboard, TouchableOpacity,
-    KeyboardAvoidingView, Linking
+    KeyboardAvoidingView, Linking, Dimensions, ActivityIndicator, ScrollView
 } from 'react-native'
 import { LinearGradient } from 'expo';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 import { createStackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import { fetchApi } from '../services/api/index';
 import { saveSession } from '../services/auth';
 
+const { width, height } = Dimensions.get('window');
 class AuthListScreen extends Component {
     constructor(props) {
       super(props);
 
       this.state={
           username: '',
+          authList: [],
+          filteredAuthList: [],
+          isLoading: false,
+          tabSelected: 1,
       }
     }
 
     componentDidMount() {
-        let user_name = this.props.navigation.state.params.userName;
+        //let user_name = this.props.navigation.state.params.userName;
+        this.setState({
+            isLoading: true,
+        })
         fetchApi({
             url: 'auth/list',
             payload: {
-                'username': this.props.userName,
+                'username': 'api_overview_testing1',
             },
             method: 'post'
         })
         .then(response => {
             console.log('Response-->', response);
-        // Once a pending authorization session object is returned
-        // if (response.message...)
+            this.setState({
+                isLoading: false,
+                authList: response.sessions,
+                filteredAuthList: response.sessions,
+            })
         })
         .catch(e => {
             this.setState({
@@ -41,10 +53,125 @@ class AuthListScreen extends Component {
             });
         });
     }
+    select = (val) => {
+        this.setState({
+            tabSelected: val,
+        })
+        if( val === 1 ) {
+            this.setState({
+                filteredAuthList: this.state.authList,
+            })
+        }
+        if( val === 2 ) {
+            let result = this.state.authList.filter(auth => {
+                return auth.status === 'approved';
+            });
+            this.setState({
+                filteredAuthList: result,
+            });
+        }
+        if( val === 3) {
+            let result = this.state.authList.filter(auth => {
+                return auth.status === 'pending'; // Expired but pending for test
+            }); 
+            this.setState({
+                filteredAuthList: result,
+            })
+        }
+        console.log(val);
+    }
+    
+    getTime = (date) => {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes +  ampm;
+        return strTime;
+    }
+
+    getDate = (date) => {
+        var _date = date.getDate();
+        var month = date.getMonth() + 1; //Be careful! January is 0 not 1
+        var year = date.getFullYear();
+        var strDate = _date + '.' + month + '.' + year;
+        return strDate;
+    }
+
+    selectItem = (item) => {
+        console.log('hhh', item);
+        this.props.navigation.navigate('AuthDetails', { item: item });
+    }
+
+    navBar = () => {
+        // this.props.navigation.navigate('Drawer');
+    }
     render() {
       return (
           <LinearGradient  colors={['#0499ED', '#0782c6', '#1170a3']} style={styles.container}>
-              
+              <View style={styles.header}>
+                {/* <TouchableOpacity onPress={this.navBar}><Text>Navbar</Text></TouchableOpacity> */}
+              </View>
+              <View style={styles.searchBar}>
+                <Ionicons style={{ alignSelf: 'center',}}name="ios-search" size={25} color="black"/>
+                <TextInput style={styles.searchInput}/>
+              </View>
+              <View style={styles.tabView}>
+                  <View style={styles.tabHeader}>
+                    <TouchableOpacity onPress={() =>this.select(1)} style={ this.state.tabSelected === 1 ?
+                     [styles.tab, {borderBottomColor: 'white', borderBottomWidth: 3}] :
+                     styles.tab
+                    }>
+                        <Text style={styles.tabText}>All</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() =>this.select(2)} style={ this.state.tabSelected === 2 ?
+                     [styles.tab, {borderBottomColor: 'white', borderBottomWidth: 3}] :
+                     styles.tab
+                    }>
+                        <Text style={styles.tabText}>Approved</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() =>this.select(3)} style={ this.state.tabSelected === 3 ?
+                     [styles.tab, {borderBottomColor: 'white', borderBottomWidth: 3}] :
+                     styles.tab}>
+                        <Text style={styles.tabText}>Expired</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.tabContent}>
+                        {this.state.isLoading && <ActivityIndicator size="large" color="white"/>}
+                        
+                        <ScrollView style={{ paddingBottom: 20 }}>
+                        <View>
+                        {this.state.filteredAuthList.map((item, i) => {
+                            var date = new Date(item.modified*1000);
+                            var time = this.getTime(date);
+                            var _date = this.getDate(date);
+                            return (
+                                <View style={styles.item} key={i}>
+                                    <View style={{flex: 1, flexDirection: 'row'}}>
+                                        <View style={{ width: 45, height: 45, borderRadius: 50, backgroundColor: 'white'}}></View>
+                                        <View style={styles.content}>
+                                            <Text style={styles.name}>{item.app_url}</Text>  
+                                            <Text style={styles.date}>{_date + ' / ' + time}</Text>                                  
+                                        </View>
+                                    </View>
+                                    <View style={styles.view}>
+                                        <TouchableOpacity onPress={() => this.selectItem(item)} style={{ marginRight: 30,}}>
+                                            <Text style={{ color: 'white', fontSize: 15, }}>View</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => this.selectItem(item)} style={{ marginRight: 10, }}>
+                                            <Entypo style={{ alignSelf: 'center',}} name="chevron-small-right" size={25} color="white"/>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            )})
+                        }
+                        </View>
+                        </ScrollView>
+                        
+                  </View>
+              </View>
           </LinearGradient>
         )
       }
@@ -57,79 +184,74 @@ const styles = StyleSheet.create({
         backgroundColor: '#1899cc',
         flexDirection: 'column',
     },
-    logoContainer: {
-        alignItems: 'center',
-        marginTop: '10%',
+    header: {
+        marginTop: '5%',
+        height: 60,
     },
-    username: {
-        color: 'white',
-        paddingVertical: 20,
-        fontSize: 25,
-        textAlign: 'center',
-        marginTop: 5,
-    },
-    infoContainer: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        height: 200,
-        padding: 20,
-        marginBottom: 0
-    },
-    input: {
-        height: 40,
-        backgroundColor: 'rgba(255,255,255,0.2)',
-        color: '#FFF',
-        marginBottom: 15,
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        fontSize: 20
-    },
-    buttonContainer: {
+    searchBar: {
+        marginTop: 20,
         backgroundColor: 'white',
-        paddingVertical: 15,
-        marginBottom: 0,
-        borderRadius: 10
+        marginHorizontal: '5%',
+        borderRadius: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        paddingVertical: 8,
     },
-    buttonText: {
-        textAlign: 'center',
-        color :'rgb(32, 53, 70)',
-        fontWeight: 'bold',
-        fontSize: 18
+    searchInput: {
+        marginLeft: 20,
+        width: width - 120,
     },
-    image: {
-      height: 100,
-      width: 100,
-      marginBottom: '5%'
+    tabView: {
+        marginTop: 20,
+        paddingBottom: 20,
     },
-    explanation: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: '300',
+    tabHeader: {
+        paddingHorizontal: '20%',
+        
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 2,
+        borderColor: 'rgba(255,255,255,0.6)',
     },
-    explanation_or: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: '300',
-      marginTop: '5%'
+    tab: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
     },
-    explanationBold: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 18
+    tabText: {
+        color: 'white'
     },
-    explanationBold_one: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 18,
-      marginTop: '5%'
+    tabContent: {
+        paddingVertical: 20,
+        marginBottom: 20,
     },
-    explanation_top: {
-      marginTop: '5%',
-      color: 'white',
-      fontSize: 18,
-      fontWeight: '300'
+    item: {
+        flex: 1,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        paddingHorizontal: 5,
+        paddingVertical: 8,
+        backgroundColor: '#2EA2E1',
+        borderRadius: 30,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    content: {
+        marginLeft: 15,
+    },
+    name: {
+        fontSize: 20,
+        color: 'white',
+    },
+    date: {
+        fontSize: 12,
+        color: 'rgba(255,255,255,0.8)'
+    },
+    view: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flexDirection: 'row',
     }
 });
 
