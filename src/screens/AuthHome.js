9048fb0db3521de6b53f2bd9e5aef779644661cc
milosristic124 <kungfupo124@gmail.com>
2018-09-25 20:41:14 +0200

    add refresh to authhome

--- src/screens/AuthHome.js
+++ src/screens/AuthHome.js
@@ -6,6 +6,7 @@ import {
     KeyboardAvoidingView, Linking
 } from 'react-native'
 import { LinearGradient } from 'expo';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 import { DrawerActions } from 'react-navigation';
 import { connect } from 'react-redux';
 import { fetchApi } from '../services/api/index';
@@ -35,7 +36,7 @@ class AuthHomeScreen extends Component {
             method: 'post'
         })
         .then(response => {
-            console.log('Response-->', response);
+            console.log('Request response-->', response);
         // Once a pending authorization session object is returned
         // if (response.message...)
         })
@@ -46,36 +47,36 @@ class AuthHomeScreen extends Component {
             });
         });
 
-        var timerId = setInterval( () => {
-            console.log('Timer');
-            fetchApi({
-                url: 'auth/get',
-                payload: {
-                    username: this.props.userName
-                },
-                method: 'post',
-            })
-                .then(response => {
-                    console.log('Response-->', response);
-                // Once a pending authorization session object is returned
-                // if (response.message...)
-                    if(response.session) {
-                        let session_id = response.session.session_id;
-                        saveSession(session_id);
-                        clearInterval(timerId);
-                        this.props.navigation.navigate('AuthApproval');
-                    }
-                    this.setState({
-                        loading: false,
-                    });
-                })
-                .catch(e => {
-                    this.setState({
-                        loading: false,
-                        errors: true,
-                    });
-                });
-          },10000);
+        // var timerId = setInterval( () => {
+        //     console.log('Timer');
+        //     try {fetchApi({
+        //         url: 'auth/get',
+        //         payload: {
+        //             username: this.props.userName
+        //         },
+        //         method: 'post',
+        //     })
+        //         .then(response => {
+        //             console.log('Timer Response-->', response);
+        //             if(response.session) {
+        //                 let session_id = response.session.session_id;
+        //                 saveSession(session_id);
+        //                 clearInterval(timerId);
+        //                 this.props.navigation.navigate('AuthApproval');
+        //             }
+        //             this.setState({
+        //                 loading: false,
+        //             });
+        //         })
+        //         .catch(e => {
+        //             this.setState({
+        //                 loading: false,
+        //                 errors: true,
+        //             });
+        //         });} catch(e) {
+        //             Alert.alert(e);
+        //         }
+        //   },10000);
 
     }
     onPress = () => {
@@ -107,9 +108,37 @@ class AuthHomeScreen extends Component {
         this.props.navigation.dispatch(DrawerActions.openDrawer());
     }
 
+    reFresh = () => {
+        try {fetchApi({
+                url: 'auth/get',
+                payload: {
+                    username: this.props.userName
+                },
+                method: 'post',
+            })
+                .then(response => {
+                    console.log('Timer Response-->', response);
+                    if(response.session) {
+                        let session_id = response.session.session_id;
+                        saveSession(session_id);
+                        this.props.navigation.navigate('AuthApproval');
+                    }
+                    this.setState({
+                        loading: false,
+                    });
+                })
+                .catch(e => {
+                    this.setState({
+                        loading: false,
+                        errors: true,
+                    });
+                });} catch(e) {
+                    Alert.alert(e);
+                }
+    }
     render() {
       return (
-          <LinearGradient  colors={['#0499ED', '#0782c6', '#1170a3']} style={styles.container}>
+          <LinearGradient  colors={['#0499ED', '#0782c6', '#1170a3']} style={styles.mainContainer}>
               <StatusBar barStyle="light-content" />
               <View style={styles.header}>
                 <TouchableOpacity style={{ marginLeft: 5 }} onPress={this.navBar}>
@@ -123,33 +152,27 @@ class AuthHomeScreen extends Component {
                       <View style={styles.logoContainer}>
                           <View style={styles.logoContainer}>
                             <Image style={styles.image}
-                              source={require('../assets/fingerprint.png')}
+                              source={require('../assets/Logo_small.png')}
                             />
-
                             <Text style={styles.username}>{this.props.userName ? this.props.userName : this.state.username}</Text>
                           </View>
                           <Text style={styles.explanation}> There are two ways to</Text>
                           <Text style={styles.explanation}> login to a dApp with your</Text>
                           <Text style={styles.explanation}> TapTrust account</Text>
-
                           <Text style={styles.explanationBold_one}> Use a dApp with a</Text>
                           <Text style={styles.explanationBold}> "TapTrust Login" option</Text>
-
                           <Text style={styles.explanation_or}> - or- </Text>
-
                           <Text style={styles.explanationBold_one}> Use the TapTrust</Text>
                           <Text style={styles.explanationBold}> browser extension</Text>
-
                           <Text style={styles.explanation_top}> Make sure this screen is </Text>
                           <Text style={styles.explanation}> visible while logging in</Text>
                           <Text style={{color: 'white', marginTop: '10%', textDecorationLine: 'underline'}}
-                            onPress={() => Linking.openURL('http://taptrust.com/about')}>
-                            Learn more about using TapTrust
+                            onPress={() => Linking.openURL('http://taptrust.com/about')}>Learn more about using TapTrust
                             </Text>
+                            <TouchableOpacity onPress={this.reFresh} style={{ marginRight: 10, marginTop: 10 }}>
+                            <Ionicons style={{ alignSelf: 'center',}} name="md-refresh" size={30} color="white"/>
+                            </TouchableOpacity>
                           </View>
-
-
-
                   </TouchableWithoutFeedback>
 
           </LinearGradient>
@@ -159,10 +182,13 @@ class AuthHomeScreen extends Component {
 
 
 const styles = StyleSheet.create({
-    container: {
+    mainContainer: {
         flex: 1,
         backgroundColor: '#1899cc',
         flexDirection: 'column',
+    },
+    container: {
+        
     },
     header: {
         marginTop: 20,
@@ -171,7 +197,7 @@ const styles = StyleSheet.create({
     },
     logoContainer: {
         alignItems: 'center',
-        marginTop: '10%',
+        marginTop: 10,
     },
     username: {
         color: 'white',
@@ -211,9 +237,9 @@ const styles = StyleSheet.create({
         fontSize: 18
     },
     image: {
-      height: 100,
-      width: 100,
-      marginBottom: '5%'
+    //   height: 100,
+    //   width: 100,
+    //   marginBottom: '5%'
     },
     explanation: {
       color: 'white',
