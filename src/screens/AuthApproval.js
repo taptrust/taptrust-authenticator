8bf2e577d9c85bdf91f05428098439b50430bec3
James L <thelevybreaks@gmail.com>
2018-11-28 12:21:01 -0800

    minor changes to AuthApproval and AuthHome

--- src/screens/AuthApproval.js
+++ src/screens/AuthApproval.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import {
-  View,  
+  View,
   StyleSheet,
   Text,
   Image,
   StatusBar,
-  TouchableOpacity,   
+  TouchableOpacity,
 } from 'react-native';
 
 import { LinearGradient } from 'expo';
@@ -30,9 +30,11 @@ class AuthApprovalScreen extends Component {
   }
 
   async componentDidMount() {
-    const request = this.props.navigation.state.params.request;
-    console.log('Request-->', request);
-    await this.setState({
+
+  /*
+  const request = this.props.navigation.state.params.request;
+  console.log('Request-->', request);
+  await this.setState({
       app_name: request.app.name,
       eth_amount: request.value,
       icon_url: request.app.icon_url,
@@ -40,10 +42,20 @@ class AuthApprovalScreen extends Component {
       app_url: 'https://' + request.app.url,
       recipient: request.recipient,
       type: request.type,
-    })
+    }) */
+    await this.setState({
+        app_name: 'Example App',
+        eth_amount: '0.01',
+        icon_url: "../assets/0x-icon.png",
+        hours_left: 0,
+        app_url: 'https://www.example.com',
+        recipient: '0x0eEB66338d9672Ba67a4342ECE388E4026f9b43d',
+        type: 'transaction',
+      })
   }
 
   onApprove = async () => {
+    return;
     await this.setState({
       formData: {
         session_id: this.props.session_id,
@@ -70,6 +82,7 @@ class AuthApprovalScreen extends Component {
   }
 
   onReject = async () => {
+    return;
     await this.setState({
       formData: {
         session_id: this.props.session_id,
@@ -94,32 +107,67 @@ class AuthApprovalScreen extends Component {
       });
   }
 
+  ellipsisHeader = (str) => {
+		let head = str.substring(0,4);
+		return head;
+	}
+
+	ellipsisTail = (str) => {
+		let length = str.length;
+		let tail = str.substring(length-5, length);
+		return tail;
+	}
+
   render() {
+
+    let authApprovalInner;
+
+    if (this.state.type === 'transaction'){
+      authApprovalInner = (
+        <View>
+        <Text style={styles.explanation}>Wants to send a transaction</Text>
+        <Text style={styles.explanation}>of the following amount:</Text>
+
+          <View style={styles.details}>
+            <Text style={styles.ethAmount}>{this.state.eth_amount} ETH</Text>
+            <View style={styles.recipient}>
+              <View>
+                <Text style={styles.toString}>will be sent to</Text>
+              </View>
+              <View style={{marginTop: 7, flexDirection: 'row'}}>
+                <Text style={styles.recipientAddress}>{this.ellipsisHeader(this.state.recipient)}</Text>
+                <Text style={styles.recipientAddress}>...</Text>
+                <Text style={styles.recipientAddress}>{this.ellipsisTail(this.state.recipient)}</Text>
+              </View>
+            </View>
+          </View>
+
+        </View>
+      );
+    }else{
+      authApprovalInner = (
+        <View>
+        <Text style={styles.explanation}> Is Requesting a</Text>
+        <Text style={styles.explanation}>Permission:</Text>
+        <View style={styles.details}>
+          <Text style={styles.ethAmount}> Spend up to {this.state.eth_amount} ETH </Text>
+          <Text style={styles.hoursLeft}> Expires in {this.state.hours_left} hours </Text>
+        </View>
+        </View>
+      );
+    }
+
     console.log('User name-->', this.props.userName);
     return (
       <LinearGradient  colors={['#0499ED', '#0782c6', '#1170a3']} style={styles.container}>
-        <Header left="back" right={false}/>
+        <Header left="back" right={false} backTo="Help" />
         <View style={styles.content}>
           <View style={styles.logoContainer}>
-            <Image style={styles.image} resizeMode="contain" source={{ uri: this.state.icon_url }}/>
+            <Image style={styles.image} resizeMode="contain" source={require('../assets/Ethroulette.png')} />
           </View>
           <View style={styles.infoContainer}>
             <Text style={styles.title}>{this.state.app_name}</Text>
-            <Text style={styles.explanation}> Is Requesting the following </Text>
-            <Text style={styles.explanation}> { this.state.type === 'transaction' ? 'Transaction' : 'Permissions:' }</Text>
-            {this.state.type === 'transaction' ?
-              <View style={styles.details}>
-                <Text style={styles.ethAmount}>{this.state.eth_amount} ETH </Text>
-                <View style={styles.recipient}>
-                  <Text style={styles.toString}>to </Text>
-                  <Text style={styles.recipientAddress}>{this.state.recipient}</Text>                        
-                </View>
-              </View> :
-              <View style={styles.details}>
-                <Text style={styles.ethAmount}> Spend up to {this.state.eth_amount} ETH </Text>
-                <Text style={styles.hoursLeft}> Expires in {this.state.hours_left} hours </Text>
-              </View>
-            }
+            {authApprovalInner}
             <Text style={styles.explanation}> Please check you are using the</Text>
             <Text style={styles.explanation}> verified {this.state.app_name} hosted at </Text>
             <Text style={styles.explanation_url}>{this.state.app_url}</Text>
@@ -128,12 +176,12 @@ class AuthApprovalScreen extends Component {
             <TouchableOpacity
               style={styles.buttonContainer}
               onPress={this.onApprove}>
-              <Text style={styles.buttonText}>Approve</Text>
+              <Text style={styles.buttonText}>Approve Request</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.buttonContainer}
               onPress={this.onReject}>
-              <Text style={styles.buttonText}>Reject</Text>
+              <Text style={styles.buttonText}>Reject Request</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -153,7 +201,7 @@ const styles = StyleSheet.create({
     paddingTop: 10,
   },
   content: {
-    marginTop: 50,
+    marginTop: 20,
   },
   logoContainer: {
     alignItems: 'center',
@@ -184,12 +232,12 @@ const styles = StyleSheet.create({
     fontWeight: '400'
   },
   details: {
-    paddingTop: 30,
-    paddingBottom: 30,
+    paddingTop: 20,
+    paddingBottom: 20,
   },
   ethAmount: {
     color: 'white',
-    fontSize: 21,
+    fontSize: 30,
     fontWeight: '300',
     textAlign: 'center',
   },
@@ -201,28 +249,29 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   recipient: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center'
   },
   toString: {
     color: 'white',
-    marginTop: 15,
+    marginTop: 5,
     fontSize: 15,
     fontWeight: '300',
   },
   recipientAddress: {
     color: 'white',
-    marginTop: 15,
     fontSize: 16,
     fontWeight: 'bold',
+    marginLeft: 5,
   },
   explanation_url: {
     textAlign: 'center',
     color: 'white',
-    fontWeight: '700'
+    fontWeight: '700',
+    lineHeight:30,
   },
   bothButtonContainer: {
-    paddingTop: 30,
+    paddingTop: 50,
   },
   buttonContainer: {
     backgroundColor: 'white',
@@ -230,7 +279,7 @@ const styles = StyleSheet.create({
     marginRight: '20%',
     paddingTop: 10,
     paddingBottom: 10,
-    marginBottom: 20,
+    marginBottom: 30,
     borderRadius: 20,
   },
   buttonText: {
